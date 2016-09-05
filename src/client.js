38729337acb45044f95aeccc1017dde4c409fd88
Alexander Nerush <ale.nerush@gmail.com>
2016-09-05 23:11:49 +0300

    Routes refactoring

--- src/client.js
+++ src/client.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { Router } from 'react-router'
+
 import routes from './routes'
+import configureStore from './store'
+
+// TODO: load data from store...
+// TODO universal app: const defaultState = window.__INIT_STATE__
+const DEFAULT_STATE = {
+  items: [
+    {
+      place: 'Barcelona'
+    },
+    {
+      place: 'Kiev'
+    },
+    {
+      place: 'Istanbul'
+    }
+  ]
+};
+
+const { store, history } = configureStore(DEFAULT_STATE)
 
 render(
-  routes,
+  <Provider store={store}>
+    <Router history={history}>
+      {routes}
+    </Router>
+  </Provider>
+  ,
   document.getElementById('root')
 )
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import { Provider } from 'react-redux'
+import { Route, IndexRoute, browserHistory } from 'react-router'
 
 // Components:
 import App from 'components/app'
@@ -9,49 +8,17 @@ import Hello from 'containers/hello'
 import Weather from 'containers/weather'
 import WeatherApp from 'containers/weatherApp'
 
-// Store
-import configureStore from './store'
-
 // Test
 // import { getWeatherByCity } from './api'
 // getWeatherByCity('London').then(data => console.log(data))
 
-// TODO: load data from store...
-// TODO universal app: const defaultState = window.__INIT_STATE__
-const DEFAULT_STATE = {
-  items: [
-    {
-      place: 'Barcelona'
-    },
-    {
-      place: 'Kiev'
-    },
-    {
-      place: 'Istanbul'
-    }
-  ]
-};
-
-const { store, history } = configureStore(DEFAULT_STATE)
-
 export default (
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={Layout}>
-        <IndexRoute component={App} />
-        <Route path="/hello/:phrase" component={Hello} />
-        <Route path="/weather" component={WeatherApp}>
-          <IndexRoute component={WeatherApp} />
-          <Route path="/weather/:city" component={Weather} />
-        </Route>
-      </Route>
-    </Router>
-  </Provider>
-)
-
-/*
-<Route path="/" component={Layout}>
-    <IndexRoute component={Home} />
-    <Route path="test" component={Test} />
+  <Route path="/" component={Layout}>
+    <IndexRoute component={App} />
+    <Route path="/hello/:phrase" component={Hello} />
+    <Route path="/weather" component={WeatherApp}>
+      <IndexRoute component={WeatherApp} />
+      <Route path="/weather/:city" component={Weather} />
+    </Route>
   </Route>
-*/
+)
